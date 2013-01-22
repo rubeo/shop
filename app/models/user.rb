6332d57b5cdee7d85e90d926a7688c25afc6b921
Serge Cat <sezya.kot@gmail.com>
2013-01-22 14:28:50 +0200

    DB_migration_add_salt_to_user_edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :password
+  attr_accessor :password, :confirmPassword
   attr_accessible :login_name, :first_name, :lastName, :password, :confirmPassword, :email, :region, :role
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
